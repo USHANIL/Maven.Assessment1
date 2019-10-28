082d6f071e540feac8c5082da54847e055b7bfb9
ushanil <ush.gopi@gmail.com>
2019-10-28 14:48:48 -0400

    Palindrome - still need to work

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -4,6 +4,23 @@ public class Palindrome {
 
     public Integer countPalindromes(String input){
 
-        return null;
+        char[] chArray = input.toCharArray();
+        int noOfPalindromes = 0;
+        int arrayLength = chArray.length-1;
+
+        for(int i=0; i< arrayLength; i++ )
+        {
+            if (chArray[i] == chArray[i+1])
+            {
+                 noOfPalindromes++;
+            }
+        }
+
+        if (chArray[arrayLength] == chArray[0])
+        {
+            noOfPalindromes++;
+        }
+
+        return noOfPalindromes + chArray.length;
     }
 }
