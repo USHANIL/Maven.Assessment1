0af934a6a40e60cde826c70019d32b5e925e6908
ushanil <ush.gopi@gmail.com>
2019-10-28 10:38:19 -0400

    IntegerUtils and IntegerArrayUtils

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
@@ -3,6 +3,8 @@ package com.zipcodewilmington.assessment1.part1;
 /**
  * Created by leon on 2/16/18.
  */
+
+import com.zipcodewilmington.assessment1.part1.BasicStringUtils;
 public class IntegerUtils {
 
 
@@ -11,7 +13,12 @@ public class IntegerUtils {
      * @return the sum of all integers between 0 and not including `n`
      */
     public static Integer getSumOfN(Integer n) {
-        return null;
+        int sum = 0;
+        for (int i=1 ; i <= n; i++)
+        {
+            sum += i;
+        }
+        return sum;
     }
 
     /**
@@ -19,7 +26,12 @@ public class IntegerUtils {
      * @return the product of all integers between 0 and not including `n`
      */
     public static Integer getProductOfN(Integer n) {
-        return null;
+        int product = 1;
+        for (int i=1 ; i <= n; i++)
+        {
+            product *= i;
+        }
+        return product;
     }
 
     /**
@@ -27,6 +39,7 @@ public class IntegerUtils {
      * @return integer with identical digits in the reverse order
      */
     public static Integer reverseDigits(Integer val) {
-        return null;
+
+        return Integer.valueOf(BasicStringUtils.reverse(val.toString()));
     }
 }
